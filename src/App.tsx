3efa29161dd37a3f0c3333d133c3d7a5408c27c3
Andrey Maximov <arman.gen40@mail.ru>
2025-06-03 19:07:06 +0300

    Add PlaceMark on map, change APIKEY, complete functions handleGeoResult and handleClickMap

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { YMaps } from "@pbe/react-yandex-maps"
 import GeocodeMap from "./components/Layout/Geocode-map"
 import config from "./config/config.json"
 
+//load: "package.full" 
+
 function App() {
   return (
     <YMaps query={{apikey: config.YANDEX_API_KEY}}>
--- src/components/Layout/Geocode-map.tsx
+++ src/components/Layout/Geocode-map.tsx
@@ -1,5 +1,18 @@
-import { Map } from "@pbe/react-yandex-maps";
+import { Map, useYMaps, Placemark } from "@pbe/react-yandex-maps";
+import { useState } from "react";
+import { type IGeocodeResult } from "yandex-maps";
 import styled from "styled-components";
+
+type CoordinatesType = Array<number>;
+
+interface IMapClickEvent {
+  get: (key: string) => CoordinatesType;
+}
+
+interface IAddress {
+  location: string;
+  route: string;
+}
 
 const MapStyled = styled(Map)`
   width: 100%;
@@ -10,9 +23,49 @@ const center = [55.739172009448396, 52.395032611328105];
 const zoom = 12;
 
 const GeocodeMap = () => {
+  const [coordinates, setCoordinates] = useState<CoordinatesType | null>(null);
+  const [address, setAddress] = useState<IAddress | null>(null);
 
-  const handleClickMap = () => {
-    console.log("click map")
+  const ymaps = useYMaps(["geocode"]);
+
+  const handleClickMap = (e: IMapClickEvent) => {
+    const coords = e.get("coords");
+
+    if (coords) {
+      setCoordinates(coords);
+    }
+
+    ymaps
+      ?.geocode(coords)
+      .then((result) => {
+        const foundAddress = handleGeoResult(result);
+        console.log("handleGeoResult", foundAddress);
+        if (foundAddress) setAddress(foundAddress);
+      })
+      .catch((error: unknown) => {
+        console.log("Ошибка геокодирования", error);
+        setAddress(null);
+      });
+
+    console.log("click map", e.get("coords"));
+  };
+
+  function handleGeoResult(result: IGeocodeResult) {
+    const firstGeoObject = result.geoObjects.get(0); // релевантный объект, который соответствует запросу
+
+    if (firstGeoObject) {
+      const properties = firstGeoObject.properties; // извлекаем из него свойства
+
+      const location = String(properties.get("description", {}));
+      const route = String(properties.get("name", {}));
+
+      const foundAddress = {
+        location,
+        route,
+      };
+
+      return foundAddress;
+    }
   }
 
   return (
@@ -21,8 +74,10 @@ const GeocodeMap = () => {
         center,
         zoom,
       }}
-      onClick={handleClickMap}
-    />
+      onClick={(e: IMapClickEvent) => handleClickMap(e)}
+    >
+      {coordinates && <Placemark geometry={coordinates} />}
+    </MapStyled>
   );
 };
 
