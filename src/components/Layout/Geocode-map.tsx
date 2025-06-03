143ca65027fed277bf6d443ebfcc22bcf38b1d0c
Andrey Maximov <arman.gen40@mail.ru>
2025-06-03 20:43:44 +0300

    The panorama object has been added. Changed the REAMDE file

--- src/components/Layout/Geocode-map.tsx
+++ src/components/Layout/Geocode-map.tsx
@@ -1,7 +1,8 @@
-import { Map, useYMaps, Placemark } from "@pbe/react-yandex-maps";
+import { Map, useYMaps, Placemark, Panorama } from "@pbe/react-yandex-maps";
 import { useState } from "react";
 import { type IGeocodeResult } from "yandex-maps";
-import { Flex, Typography } from "antd";
+import { Divider, Flex, Typography } from "antd";
+import { FrownOutlined } from "@ant-design/icons";
 import styled from "styled-components";
 
 type CoordinatesType = Array<number>;
@@ -17,32 +18,72 @@ interface IAddress {
 
 const CardWithGeocodeMap = styled(Flex)`
   width: 100%;
-  height: 700px;
+  height: 500px;
+
   gap: 6px;
 `;
 
 const MapWithGeocode = styled(Map)`
   width: 75%;
+
   border: 1px solid black;
   border-radius: 10px;
+
   overflow: hidden;
 `;
 
 const LocationInfoCard = styled(Flex)`
   width: 25%;
+
   justify-content: center;
   align-items: center;
+
   border: 1px solid black;
   border-radius: 10px;
+
+  padding: 6px;
 `;
 
 const AddressWithCoordinates = styled(Flex)`
   flex-direction: column;
 `;
 
+const InfoWithPanoramaWrapper = styled(Flex)`
+  width: 100%;
+  height: 100%;
+`;
+
 const EmptyAddressMessage = styled(Typography.Title)`
   width: 100%;
   text-align: center;
+`;
+
+const PanoramaStyled = styled(Panorama)`
+  width: 100%;
+  height: 100%;
+
+  /* margin: 6px 0; */
+  border-radius: 10px;
+
+  overflow: hidden;
+`;
+
+const NonPanoramaWrapper = styled(Flex)`
+  width: 100%;
+  height: 100%;
+
+  font-size: 16px;
+  text-align: center;
+
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  border-radius: 10px;
+  border: 1px solid grey;
+
+  background-color: #f0f0f0;
+  color: #999;
 `;
 
 const center = [55.739172009448396, 52.395032611328105];
@@ -51,6 +92,7 @@ const zoom = 12;
 const GeocodeMap = () => {
   const [coordinates, setCoordinates] = useState<CoordinatesType | null>(null);
   const [address, setAddress] = useState<IAddress | null>(null);
+  const [hasPanorama, setHasPanorama] = useState<boolean>(false);
 
   const formattedCoordinates = coordinates
     ? `${coordinates[0]?.toFixed(6)}, ${coordinates[1]?.toFixed(6)}`
@@ -64,6 +106,18 @@ const GeocodeMap = () => {
     if (coords) {
       setCoordinates(coords);
     }
+
+    //Для того, чтобы панорама обновлялась
+    ymaps?.panorama
+      .locate(coords)
+      .then((panorama) => {
+        setHasPanorama(!!panorama.length); // с помощью !! переворачиваем в булевое значение
+        console.log("panorama", panorama.length);
+      })
+      .catch((error) => {
+        console.log("Ошибка при поиске панорамы", error);
+        setHasPanorama(false);
+      });
 
     ymaps
       ?.geocode(coords) // ? стоит, тк ymaps может быть равен null
@@ -103,16 +157,29 @@ const GeocodeMap = () => {
     <CardWithGeocodeMap>
       <LocationInfoCard>
         {address ? (
-          <AddressWithCoordinates>
-            <Typography.Text>{`Локация: ${address?.location}`}</Typography.Text>
-            <Typography.Text>{`Адрес: ${address?.route}`}</Typography.Text>
-            <Typography.Text>{`Координаты: ${formattedCoordinates}`}</Typography.Text>
-          </AddressWithCoordinates>
+          <InfoWithPanoramaWrapper vertical>
+            <AddressWithCoordinates>
+              <Typography.Text>{`Локация: ${address?.location}`}</Typography.Text>
+              <Typography.Text>{`Адрес: ${address?.route}`}</Typography.Text>
+              <Typography.Text>{`Координаты: ${formattedCoordinates}`}</Typography.Text>
+            </AddressWithCoordinates>
+            <Divider />
+            {hasPanorama && coordinates ? (
+              <PanoramaStyled
+                key={coordinates?.join(",")}
+                defaultPoint={coordinates ?? undefined}
+              />
+            ) : (
+              <NonPanoramaWrapper>
+                <FrownOutlined style={{ fontSize: "100px"}} />
+                <Typography.Title>Панорама не найдена</Typography.Title>
+              </NonPanoramaWrapper>
+            )}
+          </InfoWithPanoramaWrapper> // В PanoramaStyled если coordinates = null, будем выводить undefined
         ) : (
           <EmptyAddressMessage>Выберете точку на карте</EmptyAddressMessage>
         )}
       </LocationInfoCard>
-
       <MapWithGeocode
         defaultState={{
           center,
--- src/main.tsx
+++ src/main.tsx
@@ -5,5 +5,6 @@ import App from './App.tsx'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode> 
+  // StrictMode строгий режим? включает дополнительные проверки и в режиме разработки выполняет 2 рендера компонента, только для useEffect и для useState. Это происходит только в режиме разработки, в проде такого не будет. После выгрузки, дублирования Panorama не будет
 )
