6fc2d00b3a7de6e0f2acd394fb47eaf4bc9a8eec
Andrey Maximov <arman.gen40@mail.ru>
2025-06-04 21:32:26 +0300

    Install Uuid, finished the Table component. Added an image of the saved points. Changed REAMDE

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# Сайт, сделанный на основе библиотеки React Yandex Maps.
+# Сайт, предоставляющий координаты, панораму и адрес метки с карты YandexMap.
 
 - [Страница на gh-pages](https://h1znt.github.io/React-yandex-map/ "Ссылка на сайт")
 
 ### Стэк технологий, используемых в процессе работы над проектом.
-> React, Vite, TypeScript.<br>
-> HTML, Styled Component, Ant Design.<br>
+> React, Vite, TypeScript, React Yandex Maps.<br>
+> HTML, Uuid, Styled Component, Ant Design.<br>
 
--- src/components/Layout/Geocode-map.tsx
+++ src/components/Layout/Geocode-map.tsx
@@ -1,8 +1,16 @@
 import { Map, useYMaps, Placemark, Panorama } from "@pbe/react-yandex-maps";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 // import type { IGeocodeResult } from "yandex-maps";
-import { Divider, Flex, Typography } from "antd";
-import { FrownOutlined } from "@ant-design/icons";
+import {
+  Button,
+  Divider,
+  Flex,
+  Table,
+  Typography,
+  type TableProps,
+} from "antd";
+import { DeleteOutlined, FrownOutlined } from "@ant-design/icons";
+import { v4 as uuidv4 } from "uuid";
 import styled from "styled-components";
 
 type CoordinatesType = Array<number>;
@@ -16,10 +24,19 @@ interface IAddress {
   route: string;
 }
 
+interface ISavedObject {
+  id: string;
+  address: IAddress | null;
+  coordinates: CoordinatesType | null;
+}
+
 const CardWithGeocodeMap = styled(Flex)`
   width: 100%;
-  height: 500px;
+  flex-direction: column;
+`;
 
+const CardWithMapWrapper = styled(Flex)`
+  height: 500px;
   gap: 6px;
 `;
 
@@ -85,6 +102,16 @@ const NonPanoramaWrapper = styled(Flex)`
   color: #999;
 `;
 
+const MapObjectsDisplay = styled(Map)`
+  width: 100%;
+  height: 400px;
+
+  overflow: hidden;
+
+  border-radius: 10px;
+  border: 1px solid grey;
+`;
+
 const center = [55.739172009448396, 52.395032611328105];
 const zoom = 12;
 
@@ -92,6 +119,7 @@ const GeocodeMap = () => {
   const [coordinates, setCoordinates] = useState<CoordinatesType | null>(null);
   const [address, setAddress] = useState<IAddress | null>(null);
   const [hasPanorama, setHasPanorama] = useState<boolean>(false);
+  const [objectArray, setObjectArray] = useState<ISavedObject[]>([]);
 
   const formattedCoordinates = coordinates
     ? `${coordinates[0]?.toFixed(6)}, ${coordinates[1]?.toFixed(6)}`
@@ -152,42 +180,158 @@ const GeocodeMap = () => {
     }
   }
 
+  const handleSaveObject = () => {
+    const localStorageObjects = localStorage.getItem("objects");
+
+    const objectArray = localStorageObjects
+      ? JSON.parse(localStorageObjects)
+      : [];
+
+    const newObject = {
+      id: uuidv4(),
+      address,
+      coordinates,
+    };
+
+    objectArray.push(newObject);
+    localStorage.setItem("objects", JSON.stringify(objectArray));
+
+    setObjectArray(objectArray);
+  };
+
+  const handleDeleteObject = (record: ISavedObject) => {
+    const localStorageObjects = localStorage.getItem("objects");
+
+    if (!localStorageObjects) return;
+
+    const parsedObjects: ISavedObject[] = JSON.parse(localStorageObjects);
+    const filteredObjects = parsedObjects.filter((obj) => obj.id !== record.id);
+
+    localStorage.setItem("objects", JSON.stringify(filteredObjects));
+    setObjectArray(filteredObjects);
+  };
+
+  // Таблица объектов
+
+  const loadSavedObjects = () => {
+    const localStorageObjects = localStorage.getItem("objects");
+
+    if (localStorageObjects) {
+      const parsedObjects = JSON.parse(localStorageObjects).map(
+        (item: ISavedObject) => ({
+          ...item,
+          key: item.id,
+        })
+      );
+
+      setObjectArray(parsedObjects);
+    } else {
+      setObjectArray([]);
+    }
+  };
+
+  useEffect(() => {
+    loadSavedObjects();
+  }, []);
+
+  const columns: TableProps["columns"] = [
+    {
+      title: "Локация",
+      dataIndex: ["address", "location"],
+      key: "address.location",
+    },
+    {
+      title: "Адрес",
+      dataIndex: ["address", "route"],
+      key: "address.route",
+    },
+    {
+      title: "Координаты",
+      dataIndex: "coordinates",
+      key: "coordinates",
+      render: (coords: number[]) => `${coords[0]}, ${coords[1]}`,
+    },
+    {
+      title: "",
+      key: "action",
+      render: (record) => (
+        <DeleteOutlined
+          onClick={() => {
+            handleDeleteObject(record);
+          }}
+          style={{ color: "red" }}
+        />
+      ),
+    },
+  ];
+
   return (
     <CardWithGeocodeMap>
-      <LocationInfoCard>
-        {address ? (
-          <InfoWithPanoramaWrapper vertical>
-            <AddressWithCoordinates>
-              <Typography.Text>{`Локация: ${address?.location}`}</Typography.Text>
-              <Typography.Text>{`Адрес: ${address?.route}`}</Typography.Text>
-              <Typography.Text>{`Координаты: ${formattedCoordinates}`}</Typography.Text>
-            </AddressWithCoordinates>
-            <Divider />
-            {hasPanorama && coordinates ? (
-              <PanoramaStyled
-                key={coordinates?.join(",")}
-                defaultPoint={coordinates ?? undefined}
-              />
-            ) : (
-              <NonPanoramaWrapper>
-                <FrownOutlined style={{ fontSize: "100px"}} />
-                <Typography.Title>Панорама не найдена</Typography.Title>
-              </NonPanoramaWrapper>
-            )}
-          </InfoWithPanoramaWrapper> // В PanoramaStyled если coordinates = null, будем выводить undefined
-        ) : (
-          <EmptyAddressMessage>Выберете точку на карте</EmptyAddressMessage>
-        )}
-      </LocationInfoCard>
-      <MapWithGeocode
+      <CardWithMapWrapper>
+        <LocationInfoCard>
+          {address ? (
+            <InfoWithPanoramaWrapper vertical>
+              <AddressWithCoordinates>
+                <Typography.Text>{`Локация: ${address?.location}`}</Typography.Text>
+                <Typography.Text>{`Адрес: ${address?.route}`}</Typography.Text>
+                <Typography.Text>{`Координаты: ${formattedCoordinates}`}</Typography.Text>
+              </AddressWithCoordinates>
+              <Divider />
+              {hasPanorama && coordinates ? (
+                <PanoramaStyled
+                  key={coordinates?.join(",")}
+                  defaultPoint={coordinates ?? undefined}
+                />
+              ) : (
+                <NonPanoramaWrapper>
+                  <FrownOutlined style={{ fontSize: "100px" }} />
+                  <Typography.Title>Панорама не найдена</Typography.Title>
+                </NonPanoramaWrapper>
+              )}
+              <Button
+                type="primary"
+                style={{ margin: "6px 0" }}
+                onClick={handleSaveObject}
+              >
+                Сохранить
+              </Button>
+            </InfoWithPanoramaWrapper> // В PanoramaStyled если coordinates = null, будем выводить undefined
+          ) : (
+            <EmptyAddressMessage>Выберете точку на карте</EmptyAddressMessage>
+          )}
+        </LocationInfoCard>
+
+        <MapWithGeocode
+          defaultState={{
+            center,
+            zoom,
+          }}
+          onClick={(e: IMapClickEvent) => handleClickMap(e)}
+        >
+          {coordinates && <Placemark geometry={coordinates} />}
+        </MapWithGeocode>
+      </CardWithMapWrapper>
+      <Table columns={columns} dataSource={objectArray} />
+      <MapObjectsDisplay
         defaultState={{
           center,
           zoom,
         }}
         onClick={(e: IMapClickEvent) => handleClickMap(e)}
       >
-        {coordinates && <Placemark geometry={coordinates} />}
-      </MapWithGeocode>
+        {objectArray.map(
+          (obj) =>
+            obj.coordinates && (
+              <Placemark
+                key={obj.id}
+                geometry={obj.coordinates}
+                properties={{
+                  balloonContent: `<strong>${obj?.address?.location}</strong><br/>${obj?.address?.route}`,
+                }}
+              />
+            )
+        )}
+      </MapObjectsDisplay>
     </CardWithGeocodeMap>
   );
 };
